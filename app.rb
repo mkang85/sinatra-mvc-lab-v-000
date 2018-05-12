730ed2bcc859139630f87a7b11a99fa8750188f9
Michael Kang <michaelkang85@gmail.com>
2018-05-12 21:52:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
+# require_relative 'config/environment'
+# require_relative 'models/piglatinizer.rb'
+#
+# class App < Sinatra::Base
+#
+#   get '/' do
+#     erb :user_input
+#   end
+#
+#   post '/piglatinize' do
+#     @piglatin = PigLatinizer.new(params[:user_phrase])
+#     erb :piglatinize
+#   end
+#
+# end
+
+
+
 require_relative 'config/environment'
-require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
 
@@ -8,8 +25,9 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @piglatin = PigLatinizer.new(params[:user_phrase])
-    erb :piglatinize
+    pl = PigLatinizer.new
+    @piglatin = pl.piglatinize(params[:user_phrase])
+    erb :results
   end
 
 end
