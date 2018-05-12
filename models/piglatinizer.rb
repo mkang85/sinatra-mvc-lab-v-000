b4ac7bb366b078dd93643dcaf4e16f5286f89541
Michael Kang <michaelkang85@gmail.com>
2018-05-12 21:05:19 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,11 +10,21 @@ class PigLatinizer
   def pigger(word)
     vowels = 'aeiouAEIOU'
     new_word = word.downcase
-    
+
     case pig
     when vowels.include(word[0])
       word + 'way'
     when new_word[0, 2] == "ch"
+      new_word.slice!(2..-1) + new_word[0,2] + 'ay'
+    when new_word[0, 2] == "sh"
+      new_word.slice!(2..-1) + new_word[0,2] + 'ay'
+    when new_word[0, 2] == "th"
+      new_word.slice!(2..-1) + new_word[0,2] + 'ay'
+    when new_word[0, 2] == "st"
+      new_word.slice!(2..-1) + new_word[0,2] + 'ay'
+    when new_word[0, 2] == "cr"
+      new_word.slice!(2..-1) + new_word[0,2] + 'ay'
+    when new_word[0, 2] == "gl"
       new_word.slice!(2..-1) + new_word[0,2] + 'ay'
     else
       word.slice!(1..-1) + word[0] + "ay"
