81fbf0f472ebdcf53f9bb5798cd46764c427431d
Michael Kang <michaelkang85@gmail.com>
2018-05-12 20:49:23 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,4 +6,21 @@ class PigLatinizer
     @text = text
   end
 
+
+  def pigger(word)
+    vowels = 'aeiouAEIOU'
+
+    case pig
+    when vowels.include(word[0])
+      word.slice(0) + "w" + word[0]
+  end
+   
+  def piglatinizer(text)
+    arr = text.split(' ')
+
+    arr.each do |word|
+      pigger(word)
+    end 
+  end 
+
 end
