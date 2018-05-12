0271b2b77b61b03be2edd9956dda403548f8c67d
Michael Kang <michaelkang85@gmail.com>
2018-05-12 21:51:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @piggedtext = PigLatinizer.new(params[:user_phrase])
+    @piglatin = PigLatinizer.new(params[:user_phrase])
     erb :piglatinize
   end
 
